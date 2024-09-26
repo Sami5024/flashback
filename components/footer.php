9018d226fc36c3e9a360fccbbaafd1a10f345f40
Sami5024 <samiirfan091@gmail.com>
2024-09-26 16:50:38 +0500

    webpage

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +923488118956</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +923420059498</a>
+         <a href="mailto:harshchy143@gmail.com"><i class="fas fa-envelope"></i>Samiirfan091@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Islamabad,Pakistan </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://www.tiktok.com/@flash_back.com?_t=8q2Q7flD4K2&_r=1" target="_blank"><i class="fab fa-twitter"></i>TikTok</a>
+         <a href="https://www.instagram.com/flash_back5024/?utm_source=qr" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/sami-irfan/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>FlashBack</span> | all rights reserved!</div>
+
+</footer>
